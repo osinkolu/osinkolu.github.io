52fcc31c033995fdfdfc96b678c0e361d564fb89
Olufemi Victor <60588823+osinkolu@users.noreply.github.com>
2021-03-17 03:18:48 +0100

    Created payments page

---
+++ paystack.js
@@ -0,0 +1,56 @@
+const paymentForm = document.getElementById('paystackpaymentForm');
+paymentForm.addEventListener("submit", payWithPaystack, false);
+function payWithPaystack(e) {
+  e.preventDefault();
+  let handler = PaystackPop.setup({
+    key: 'pk_live_6cc5a2a7f43d7bc613ab14b2d5f1b0b3a7f723c2', // Replace with your public key
+    email: document.getElementById("paystack-email-address").value,
+    amount: document.getElementById("paystack-amount").value * 100,
+    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+    // label: "Optional string that replaces customer email"
+    onClose: function(){
+      alert('Window closed.');
+    },
+    callback: function(response){
+      let message = 'Payment complete! Reference: ' + response.reference;
+      alert(message);
+    }
+  });
+  handler.openIframe();
+}
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.getElementById("submit-flutterwave").addEventListener("click", function(e) {
+    var PBFKey = "FLWPUBK-272171fed04e1d754a6517829040b921-X";
+    
+    getpaidSetup({
+      PBFPubKey: PBFKey,
+      customer_email: "user@example.com",
+      customer_firstname: "Temi",
+      customer_lastname: "Adelewa",
+      custom_description: "Pay Internet",
+      custom_logo: "http://localhost/communique-3/skin/frontend/ultimo/communique/custom/images/logo.svg",
+      custom_title: "Communique Global System",
+      amount: 2000,
+      customer_phone: "234099940409",
+      country: "NG",
+      currency: "NGN",
+      txref: "rave-123456",
+      integrity_hash: "6800d2dcbb7a91f5f9556e1b5820096d3d74ed4560343fc89b03a42701da4f30",
+      onclose: function() {},
+      callback: function(response) {
+        var flw_ref = response.tx.flwRef; // collect flwRef returned and pass to a                  server page to complete status check.
+        console.log("This is the response returned after a charge", response);
+        if (
+          response.tx.chargeResponseCode == "00" ||
+          response.tx.chargeResponseCode == "0"
+        ) {
+          // redirect to a success page
+        } else {
+          // redirect to a failure page.
+        }
+      }
+    });
+  });
+});
